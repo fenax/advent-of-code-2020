87cf9429d63c33bae6460e426142dcfa8b0bb081
Kasil <kasil@loutre.info>
2020-12-10 11:35:59 +0100

    day 9 part 2 is good too.

--- src/day09.rs
+++ src/day09.rs
@@ -1,6 +1,9 @@
 use super::*;
 use super::puzzles::Data;
 use std::vec::Vec;
+use itertools::Itertools;
+
+use itertools::MinMaxResult::{NoElements, OneElement, MinMax};
 
 #[derive(Clone)]
 pub struct Input{
@@ -42,7 +45,7 @@ impl<'a> std::fmt::Display for Part2<'a>{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self.solution{
             Some(x) => {
-                write!(f, "accumulator at end is {}", x)
+                write!(f, "Weakness is {}", x)
             },
             None => {
                 write!(f, "no solution found")
@@ -72,6 +75,16 @@ where 'a:'b
 }
 
 
+fn find_contiguous(input:&Vec<i64>,target:i64)->Vec<i64>{
+    for i in 0..input.len(){
+        'inner: for j in i+1..input.len(){
+            let sum :i64 = input[i..=j].iter().sum();
+            if sum > target{ break 'inner;}
+            if sum == target{ return input[i..=j].to_vec()}
+        }
+    }
+    Vec::new()
+}
 
 
 impl<'a,'b> puzzles::Puzzle<'a, Input> for Part2<'b>
@@ -81,6 +94,17 @@ where 'a:'b
         Part2{data:input,solution:None}
     }
     fn resolve(&mut self){
+        let mut part1 = day09::Part1::new(self.data);
+        part1.resolve();
+        if let Some(target) = part1.solution{
+            println!("target is {}",target);
+            let cont = find_contiguous(self.data.get_data(),target);
+            println!("contiguous list is {:?}",cont);
+            if let MinMax(min,max) = cont.iter().minmax(){
+                self.solution = Some(min+max);
+            }
+
+        }
 
     }
 }
